ed2983cca5f76aff031d3ac814c1706500dd2fb5
EduardV777 <89710974+EduardV777@users.noreply.github.com>
2021-11-04 19:24:23 +0200

    Update main.py

--- Mini Games(Python)/Words Guesser/main.py
+++ Mini Games(Python)/Words Guesser/main.py
@@ -1,4 +1,4 @@
-import random
+from random import random
 class AccountsData:
     AccountName=["Sec2727", "JohnDoe"]; AccountPoints=[200,300]; AccountPos=[1,2]
 def ShowGuide():
@@ -11,66 +11,82 @@ def ShowGuide():
 def StartGame(name, difficulty, wordsList):
     accountId=AccountsData.AccountName.index(name)
     totalPoints=0; failed=False
+    #creating required parameters depending on the difficulty
     if difficulty=="Easy":
         wordsCount=3
         range1=0.00; range2=0.94; range3=0.95; range4=1
-        minWordLength=3;wordLength1=4; wordLength2=4
+        minWordLength=3; maxWordLength=4
         winPoints=5; losePoints=10
         guessTries=2; maxHiddenLetters=1
     elif difficulty=="Medium":
         wordsCount=5
         range1 = 0.00; range2 = 0.70; range3 = 0.71; range4 = 1
-        minWordLength=4; wordLength1 = 6; wordLength2 = 6
+        minWordLength=4; maxWordLength = 5
         winPoints = 10; losePoints = 20
         guessTries = 2; maxHiddenLetters=3
     elif difficulty=="Hard":
         wordsCount=7
-        range1 = 0.00; range2 = 0.70; range3 = 0.71; range4 = 1
-        minWordLength = 6; wordLength1 = 9; wordLength2 = 9
+        range1 = 0.00; range2 = 0.69; range3 = 0.70; range4 = 1
+        minWordLength = 6; maxWordLength = 8
         winPoints = 30; losePoints = 60
         guessTries = 1; maxHiddenLetters=3
+
     print("Choosing words . . .")
-    shortWords=[]
-    for k in range(0,len(wordsList)):
-        if len(wordsList[k])<wordLength2+1:
-            shortWords.append(wordsList[k])
+    eligibleWords=[]; t=0
+    #Filling a list with all eligible words for the difficulty level
+    while t<len(wordsList):
+        if minWordLength<=len(wordsList[t])<maxWordLength+1:
+            eligibleWords.append(wordsList[t])
+            del wordsList[t]
+            continue
+        else:
+            t+=1
     chosenWords=[]; k=0
+    #Choosing certain amount of random words that fulfill the requirement
     while k<wordsCount:
-        chance=round(random.random(),2)
+        chance=round(random(),2); noSuchWords=False
         if range1<=chance<=range2:
             #length=len(shortWords)-1
             while True:
-                randomWord=round(random.random()*len(shortWords)-1)
-                if minWordLength<=len(shortWords[randomWord])<wordLength1:
-                    chosenWords.append(shortWords[randomWord])
-                    del shortWords[randomWord]
+                randomWord=round(random()*len(eligibleWords)-1)
+                if minWordLength<=len(eligibleWords[randomWord])<maxWordLength:
+                    chosenWords.append(eligibleWords[randomWord])
+                    del eligibleWords[randomWord]
                     break
                 else:
                     continue
         elif range3<=chance<=range4:
             while True:
-                randomWord=round(random.random()%len(shortWords))
-                if len(shortWords[randomWord])==wordLength2:
-                    chosenWords.append(shortWords[randomWord])
-                    del shortWords[randomWord]
+                randomWord=round(random()*len(eligibleWords)-1)
+                if len(eligibleWords[randomWord])==maxWordLength:
+                    chosenWords.append(eligibleWords[randomWord])
+                    del eligibleWords[randomWord]
                     break
                 else:
+                    #Are there any words at all that fulfill the request?
+                    for j in range(0,len(eligibleWords)):
+                        if len(eligibleWords[j])==maxWordLength:
+                            noSuchWords=True
+                            break
+                    if noSuchWords==False:
+                        break
                     continue
         k+=1
     print("Hiding letters . . .\n\n\n")
     #print(chosenWords)
     wordN=1
+    #hiding letter, displaying words to the player and processing player's answer
     for k in range(0,len(chosenWords)):
         originalWord=chosenWords[k]; triesPerWord=guessTries
         currentWord=[]; hideLetters=maxHiddenLetters; h=0
         for j in range(0,len(chosenWords[k])):
             currentWord.append(chosenWords[k][j])
         if maxHiddenLetters>1 and difficulty!="Hard":
-            hideChance=round(random.random())
-            if 0.01<hideChance:
+            hideChance=round(random(),2)
+            if 0.05<hideChance:
                 hideLetters-=1
         while h<hideLetters:
-            hideRandLetter=round(random.random()*len(currentWord)-1)
+            hideRandLetter=round(random()*len(currentWord)-1)
             if currentWord[hideRandLetter]=="_" or hideRandLetter==0:
                 continue
             else:
@@ -99,12 +115,13 @@ def StartGame(name, difficulty, wordsList):
                 continue
         if failed==True:
             break
+    #Processing and updating account data depending on the outcome of the player's result
     if failed==False:
-        print(f"You guessed all words correctly!\nEarned points={totalPoints}")
+        print(f"\n\nYou guessed all words correctly!\nEarned points={totalPoints}")
         AccountsData.AccountPoints[accountId]+=totalPoints
         UpdateAccountStats()
     else:
-        print(f"You couldn't guess all the words.\nEarned points={totalPoints}")
+        print(f"\n\nYou couldn't guess all the words.\nEarned points = {totalPoints}\n\n")
         if totalPoints>0:
             AccountsData.AccountPoints[accountId]+=totalPoints
 def UpdateAccountStats():
@@ -128,8 +145,6 @@ def ShowAccountStats(name):
     leaderboardsPos=AccountsData.AccountPos[id]
     print(f"\nName: {name}\nTotal points: {points}\nLeaderboards position: {leaderboardsPos}\n")
 def main():
-    #randomNum=round(random.random(),2)
-    #print(randomNum)
     while True:
         name=input("Enter your name: ")
         if len(name)<2:
@@ -143,7 +158,7 @@ def main():
         print("1 - Start a new game     2 - Check your account stats\n3 - Read the guide     4 - Check the leaderboards")
         option=input()
         if option=="1":
-            words=["game","global","unreal","dictionary","month","bread","spices","potato","city","car","stop","tag","skip","loud","break","turn","will","power","day","way","stay","wanderer","deal","seal","bill","drill","still","spill","water","bottle","scratch","food"]
+            words=["game","global","unreal","dictionary","month","bread","spices","potato","city","car","stop","tag","skip","loud","break","turn","will","power","day","way","stay","wanderer","deal","seal","bill","drill","still","spill","water","bottle","scratch","food","station","destination","coronation","statement","presentation","intuition","premonition","consciousness","nationality","respectively","perspective","introspection","construction"]
             while True:
                 diff=input("Select Difficulty(Easy/Medium/Hard): ")
                 if diff!="Easy" and diff!="Medium" and diff!="Hard":
@@ -166,5 +181,4 @@ def main():
                 print(f"{accPos} -- {accName} -- {accPoints}")
             print("\n")
             continue
-
 main()
