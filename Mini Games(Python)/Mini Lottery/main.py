07cc2a5438defcd8873b815a85f983e9f894ca85
EduardV777 <89710974+EduardV777@users.noreply.github.com>
2021-11-06 21:43:06 +0200

    Update main.py

--- Mini Games(Python)/Mini Lottery/main.py
+++ Mini Games(Python)/Mini Lottery/main.py
@@ -1,6 +1,88 @@
 import mysql.connector; import time
-def LogIntoAccount(dbConnectionInfo):
+def ControlPanel(name, accId):
+    queryExecution.execute(f"Select accountBalance from accountstats where accountId='{accId}'")
+    queryRes=queryExecution.fetchone()
+    balance=queryRes[0]
+    queryExecution.execute(f"Select playedGames, wonGames, lostGames from accountstats where accountId='{accId}'")
+    queryRes = queryExecution.fetchone()
+    par1=queryRes[0]; par2=queryRes[1]; par3=queryRes[2]
+    class currentAccountStats:
+        accName=name; accBalance=float(balance)
+        playedGamesTotal=par1; wonGamesTotal=par2; lostGamesTotal=par3
+        currentlyPlayedGames=0; currentlyWonGames=0; currentlyLostGames=0
+    print(f"\n\n\n\n\n\n|||Lottery game|||\n\nWelcome, {currentAccountStats.accName}\nCurrent balance: {currentAccountStats.accBalance} BetCoins\n\n")
     while True:
+        print("1 - Bet   2 - Check account stats\n3 - Account operations   4 - Support\n5 - Log off")
+        option=input("\n- ")
+        if option=="1":
+            pass
+        elif option=="2":
+            print(f"Stats for account ID[{accId}]:\nTotal Played Games - {currentAccountStats.playedGamesTotal}\nTotal Won Games - {currentAccountStats.wonGamesTotal}\nTotal Lost Games - {currentAccountStats.lostGamesTotal}")
+            continue
+        elif option=="3":
+            while True:
+                controlOption=input("\nEnter '1' if you wish to withdraw money from your current BetCoin balance\nEnter '2' if you wish to deposit funds into your BetCoin balance\nEnter '3' if you want to change your name/password\n\n[4]Return")
+                if controlOption=="1":
+                    print(f"Your current BetCoin balance is: {currentAccountStats.accBalance}\nEnter the amount of money you'd like to withdraw:\n")
+                    withdrawAmount=float(input("- "))
+                    if withdrawAmount>currentAccountStats.accBalance:
+                        print("\nInsufficient funds!\n")
+                        break
+                    else:
+                        currentAccountStats.accBalance-=withdrawAmount
+                        print("Operation completed!")
+                        break
+                elif controlOption=="2":
+                    print("Enter the amount of money you'd like to deposit into your account:\n")
+                    depositAmount=float(input("- "))
+                    if depositAmount>10000:
+                        print("\nSorry, deposits are limited to 10 000\n")
+                        break
+                    else:
+                        currentAccountStats.accBalance+=depositAmount
+                        print("\nOperation completed!\n")
+                        break
+                elif controlOption=="3":
+                    print("Enter 'changePass' to change your password\nEnter 'changeName' to change your account name.\n\n[4]Return")
+                    changeOption=input("- ")
+                    if changeOption=='changePass':
+                        while True:
+                            currentPass=input("Enter your current password: ")
+                            queryExecution.execute(f"Select accountPassword from accounts where accountdId={accId}")
+                            queryRes=queryExecution.fetchone()
+                            realCurrentPass=queryRes[0]
+                            if currentPass!=realCurrentPass:
+                                print("\nIncorrect current password!\n")
+                                continue
+                            else:
+                                while True:
+                                    newPass=input("Enter your new password: ")
+                                    if newPass<5:
+                                        print("\nPassword is too short! It must be at least 5 symbols long.\n")
+                                        continue
+                                    elif newPass>20:
+                                        print("\nPassword is too long! It must be below 20 symbols length.\n")
+                                        continue
+                                    else:
+                                        print("\nYou have changed your password successfully!\n")
+                                        break
+                                break
+                    elif changeOption=="changeName":
+                        pass
+                    elif changeOption=="4":
+                        break
+            continue
+        elif option=="4":
+            pass
+        elif option=="5":
+            pass
+        else:
+            continue
+def LogIntoAccount(dbConnectionInfo):
+    global successfullLogin
+    successfullLogin=False
+    while True:
+        global userName; global queryExecution
         userName=input("Username: "); password=input("Password: ")
         print("\nLogging in . . .\n")
         queryExecution=dbConnectionInfo.cursor()
@@ -14,14 +96,22 @@ def LogIntoAccount(dbConnectionInfo):
             queryRes=queryExecution.fetchone()
             try:
                 passw=queryRes.index(password)
-                print("\nSuccessful login!\n")
-                break
+                #check if account is suspended in case of successful logging in
+                queryExecution.execute(f"Select accountStatus from accounts where accountName='{userName}'")
+                queryRes = queryExecution.fetchone()
+                if queryRes[0]=="suspended":
+                    print(f"\n\nThis account with name {userName} is suspended.\n If you think this is a mistake, contact the administrator\n\n")
+                    main()
+                else:
+                    print("\nSuccessful login!\n")
+                    successfullLogin=True
+                    break
             except:
                 print("\nLogin data incorrect!\n")
                 continue
-    print(queryRes)
+    #print(queryRes)
 def CreateAccount(dbConnectionInfo):
-    print("\n||Account creation||\n")
+    print("\n||Account creation||\n\n**Every new account now gets free 20 BetCoins to help your luck at getting amazing rewards!**\n")
     while True:
         accName=input("Account Name: ")
         if len(accName)<4:
@@ -46,11 +136,12 @@ def CreateAccount(dbConnectionInfo):
     currentDateTime=time.gmtime()
     queryExecution=dbConnectionInfo.cursor()
     queryExecution.execute(f"INSERT INTO accounts(accountName,accountPassword,creationDate) VALUES ('{accName}', '{accPass}', '{currentDateTime.tm_mday}.{currentDateTime.tm_mon}.{currentDateTime.tm_year} {currentDateTime.tm_hour}:{currentDateTime.tm_min}' )")
+    queryExecution.execute("INSERT INTO accountStats(playedGames,wonGames,lostGames) VALUES(0,0,0)")
     dbConnectionInfo.commit()
     print("\nAccount successfully created!\n")
 def main():
     #connect to DB
-    hostAddress="localhost"; adminAccessName="root"; adminAccessPass="root"; db="accountdata"
+    hostAddress="localhost"; adminAccessName="accountControl"; adminAccessPass="control"; db="accountdata"
     try:
         database=mysql.connector.connect(host=hostAddress, user=adminAccessName, password=adminAccessPass, database=db)
     except:
@@ -69,7 +160,27 @@ def main():
             else:
                 print("\nWrong option\n")
                 continue
+            break
         except ValueError:
             print("\nWrong option\n")
             continue
+    if successfullLogin==True:
+        queryExecution.execute(f"Select accountId from accounts where accountName='{userName}'")
+        global accountId; accountId=""
+        queryRes=queryExecution.fetchone()
+        k=0
+        #take actual id from query result
+        while k<len(queryRes):
+            if k!="(" or k!=",":
+                for j in range(k,len(queryRes)):
+                    if queryRes[j]!=",":
+                        accountId+=str(queryRes[j])
+                        k+=1
+                    else:
+                        k+=1
+                        break
+                break
+            else:
+                k+=1
+        ControlPanel(userName, accountId)
 main()
