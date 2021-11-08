9ddf6bd774597b6900864ce5ba4fc3c1730220c9
EduardV777 <89710974+EduardV777@users.noreply.github.com>
2021-11-08 14:11:56 +0200

    Update CreateUser.sql

--- Mini Games(Python)/Mini Lottery/CreateUser.sql
+++ Mini Games(Python)/Mini Lottery/CreateUser.sql
@@ -1,3 +1,4 @@
 CREATE USER 'accountControl'@'localhost' IDENTIFIED BY 'control';
 GRANT SELECT,INSERT,UPDATE ON accounts TO 'accountControl'@'localhost';
 GRANT SELECT,INSERT,UPDATE ON accountStats TO 'accountControl'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON bugreports TO 'accountControl'@'localhost'
