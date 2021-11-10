36d15e627f851cbeb5d8745b70e2eab32de6f788
EduardV777 <89710974+EduardV777@users.noreply.github.com>
2021-11-10 22:07:02 +0200

    Add files via upload

---
+++ Car Dealership/CarDealership.py
@@ -0,0 +1,17 @@
+import mysql.connector; import time
+def main():
+    print("       Ed's Car Dealership\n")
+    print("1 - Check the current car offers | 2 - Check your orders\n3 - Send special request | 4 - Contact us")
+    while True:
+        option=input("- ")
+        if option=="1":
+            pass
+        elif option=="2":
+            pass
+        elif option=="3":
+            pass
+        elif option=="4":
+            pass
+        else:
+            continue
+main()
