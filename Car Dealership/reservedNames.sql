effe3d9ca55d29da67625b0845324b5dea63f13a
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-17 15:13:30 +0200

    Update reservedNames.sql

--- Car Dealership/reservedNames.sql
+++ Car Dealership/reservedNames.sql
@@ -1,3 +1,4 @@
+#This table will hold names that will not be allowed when the user is registrating an account, but it can also hold words that will not be allowed in other cases
 CREATE TABLE reservedNames (
 reservedName VARCHAR(30) NOT NULL
 );
