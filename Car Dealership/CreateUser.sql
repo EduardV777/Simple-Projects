a64760dcc6e036cd054d1dad7e484018070af681
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-17 18:33:58 +0200

    Add files via upload
    Admin DB account

---
+++ Car Dealership/CreateUser.sql
@@ -0,0 +1,7 @@
+GRANT SELECT,INSERT,UPDATE ON accounts TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON offerPostings TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON reservednames TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON postbox TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON usernotifications TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON userorders TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE ON myoffers TO 'admin'@'localhost';
