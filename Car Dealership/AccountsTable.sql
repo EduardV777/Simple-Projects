2ba6cf87d67619d59658cef828c109b7d2c2cb9f
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-17 15:11:43 +0200

    Add files via upload

---
+++ Car Dealership/AccountsTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE accounts (
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(20) NOT NULL,
+PASSWORD VARCHAR(100) NOT NULL,
+email VARCHAR(60) NOT NULL,
+dateCreated VARCHAR(10) NOT NULL,
+rating DOUBLE DEFAULT '0.00',
+address VARCHAR(60) DEFAULT 'Not stated',
+telephone VARCHAR(10) DEFAULT 'Not stated',
+company VARCHAR(30) DEFAULT 'None',
+accountFlags INT DEFAULT 0
+);
