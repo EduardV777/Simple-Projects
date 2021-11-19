af2407eb96af23cf8cd841fa59897e57aeb066b8
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-19 18:02:12 +0200

    Update CreateUser.sql

--- Car Dealership/CreateUser.sql
+++ Car Dealership/CreateUser.sql
@@ -1,9 +1,9 @@
 CREATE USER 'admin2'@'localhost' IDENTIFIED BY 'admin';
 
-GRANT SELECT,INSERT,UPDATE ON accounts TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON offerPostings TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON reservednames TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON postbox TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON usernotifications TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON userorders TO 'admin'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON myoffers TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON accounts TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON offerPostings TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON reservednames TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON postbox TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON usernotifications TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON userorders TO 'admin'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE ON myoffers TO 'admin'@'localhost';
