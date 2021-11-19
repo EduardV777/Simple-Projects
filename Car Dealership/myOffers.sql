a4251b578df67468df7ce1bb43493258b97a49e5
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-19 14:21:36 +0200

    Update myOffers.sql
    Additional row 'askPrice' has been added.

--- Car Dealership/myOffers.sql
+++ Car Dealership/myOffers.sql
@@ -1,6 +1,7 @@
 CREATE TABLE myoffers(
 accountId INT PRIMARY KEY,
 offerId INT NOT NULL,
+askPrice VARCHAR(30) DEFAULT 'Discuss with customer',
 posted VARCHAR(20) NOT NULL,
 status VARCHAR(20) DEFAULT 'Listed'
 );
