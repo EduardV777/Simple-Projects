de2fc2f295dc174b86940d0dcb5b87ed9081a915
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-11-29 00:18:39 +0200

    Update userOrders.sql

--- Car Dealership/userOrders.sql
+++ Car Dealership/userOrders.sql
@@ -1,5 +1,6 @@
-CREATE TABLE userOrders(
-accountId INT PRIMARY KEY NOT NULL,
+CREATE TABLE userorders(
+orderId INT NOT NULL,
+accountId INT NOT NULL,
 offerId INT NOT NULL,
 title VARCHAR(100) NOT NULL,
 description VARCHAR(500) NOT NULL,
@@ -7,9 +8,9 @@ TYPE VARCHAR(30) NOT NULL,
 fuelType VARCHAR(30) NOT NULL,
 driveType VARCHAR(10)NOT NULL,
 yearProd VARCHAR(10)NOT NULL,
-posted VARCHAR(20) NOT NULL,
+posted VARCHAR(30) NOT NULL,
 ordered VARCHAR(20) NOT NULL,
 orderstatus VARCHAR(20) DEFAULT 'Unknown',
 SoldBy VARCHAR(20) NOT NULL,
-ExpectedDelivery VARCHAR(20) DEFAULT 'Waiting for seller response'
+ExpectedDelivery VARCHAR(50)
 );
