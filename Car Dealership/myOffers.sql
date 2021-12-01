3f3ee497aa4337258f7f60b16c55ee952ad8d9de
Eduard Velkov <89710974+EduardV777@users.noreply.github.com>
2021-12-01 15:20:12 +0200

    Update myOffers.sql

--- Car Dealership/myOffers.sql
+++ Car Dealership/myOffers.sql
@@ -3,6 +3,6 @@ accountId INT NOT NULL,
 offerId INT PRIMARY KEY,
 askPrice VARCHAR(30) NOT NULL,
 posted VARCHAR(20) NOT NULL,
-status VARCHAR(20) DEFAULT 'Listed',
+status VARCHAR(100) DEFAULT 'Listed',
 flags VARCHAR(200) DEFAULT ''
 );
